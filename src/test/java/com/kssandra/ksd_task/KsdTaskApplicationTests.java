dce0376d74b5ce58185d89bd4d0a5d1405232ff1
aquesada <aquesada@AQUESADA10.indra.es>
2022-05-25 07:26:06 +0200

    First commit

---
+++ src/test/java/com/kssandra/ksd_task/KsdTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kssandra.ksd_task;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KsdTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
