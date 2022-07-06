2407cbbb23e704b7109a56e1fd53b85be1528661
aquesada <aquesada@AQUESADA10.indra.es>
2022-07-06 17:35:03 +0200

    Unit testing

--- src/test/java/com/kssandra/ksd_task/provider/CryptoDataProviderTest.java
+++ src/test/java/com/kssandra/ksd_task/provider/CryptoDataProviderTest.java
@@ -3,26 +3,27 @@
  */
 package com.kssandra.ksd_task.provider;
 
-import static org.hamcrest.CoreMatchers.any;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.anyList;
-import static org.mockito.Mockito.doCallRealMethod;
-import static org.mockito.Mockito.doNothing;
-import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.assertj.core.util.Maps;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.mockito.ArgumentCaptor;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.mock.mockito.SpyBean;
 
 import com.kssandra.alphavantage_client.output.IntraDay;
 import com.kssandra.alphavantage_client.output.SimpleCryptoCurrencyData;
@@ -30,6 +31,7 @@ import com.kssandra.ksd_common.dto.CryptoCurrencyDto;
 import com.kssandra.ksd_common.dto.CryptoDataDto;
 import com.kssandra.ksd_common.exception.DataCollectException;
 import com.kssandra.ksd_persistence.dao.CryptoDataDao;
+import com.kssandra.ksd_task.provider.alphavantage.AlphaVantageCryptoDataProvider;
 
 /**
  * @author aquesada
@@ -38,73 +40,114 @@ import com.kssandra.ksd_persistence.dao.CryptoDataDao;
 @SpringBootTest
 class CryptoDataProviderTest {
 
-	@Autowired
-	CryptoDataProvider cxDataProvider;
+	@SpyBean
+	CryptoDataProvider cryptoDataProvider;
 
 	@MockBean
-	CryptoDataProvider mockDataProvider;
+	private CryptoDataDao cryptoDataDao;
 
-	@MockBean
-	private CryptoDataDao mockCxDataDao;
+	private static final String cxCode1 = "CXT1";
+
+	private static final String cxCode2 = "CXT2";
 
 	/**
 	 * Test method for
 	 * {@link com.kssandra.ksd_task.provider.CryptoDataProvider#collectIntraDayData(java.util.List)}.
-	 * 
-	 * @throws DataCollectException
 	 */
 	@Test
 	void testCollectIntraDayData() throws DataCollectException {
 
-		assertTrue(cxDataProvider.collectIntraDayData(Collections.emptyList()).isEmpty());
+		CryptoCurrencyDto cxCurr1 = new CryptoCurrencyDto(cxCode1);
+		CryptoCurrencyDto cxCurr2 = new CryptoCurrencyDto(cxCode2);
+		List<CryptoCurrencyDto> activeCxCurrs = new ArrayList<CryptoCurrencyDto>(Arrays.asList(cxCurr1, cxCurr2));
 
-		List<CryptoCurrencyDto> activeCxCurrs = new ArrayList<>();
-		CryptoCurrencyDto cxCurr1 = new CryptoCurrencyDto();
-		cxCurr1.setCode("CXT1");
-		activeCxCurrs.add(cxCurr1);
-//		CryptoCurrencyDto cxCurr2 = new CryptoCurrencyDto();
-//		cxCurr2.setCode("CXT2");
-// 		activeCxCurrs.add(cxCurr2);
+		Map<String, String> metaData1 = new HashMap<>(
+				Maps.newHashMap(AlphaVantageCryptoDataProvider.getMdataCurrencyCode(), cxCode1));
+		Map<String, String> metaData2 = new HashMap<>(
+				Maps.newHashMap(AlphaVantageCryptoDataProvider.getMdataCurrencyCode(), cxCode2));
+		List<SimpleCryptoCurrencyData> digitalData = new ArrayList<>(Arrays
+				.asList(new SimpleCryptoCurrencyData(LocalDateTime.now(), 122.38, 123.45, 119.00, 120.54, 4562987)));
+		IntraDay intraDay1 = new IntraDay(metaData1, digitalData);
+		IntraDay intraDay2 = new IntraDay(metaData2, digitalData);
 
-		// CryptoDataProvider mockDataProvider = mock(CryptoDataProvider.class);
+		doReturn(intraDay1).when(cryptoDataProvider).callService(cxCurr1);
+		doReturn(intraDay2).when(cryptoDataProvider).callService(cxCurr2);
 
-		when(mockDataProvider.callService(cxCurr1)).thenReturn(new IntraDay(null, null));
-		when(mockDataProvider.mapIntraDayRs(any(IntraDay.class))).thenReturn(new ArrayList<>());
-		doCallRealMethod().when(mockDataProvider).collectIntraDayData(activeCxCurrs);
+		CryptoDataDto dto1 = new CryptoDataDto();
+		dto1.setCxCurrencyDto(cxCurr1);
+		CryptoDataDto dto2 = new CryptoDataDto();
+		dto2.setCxCurrencyDto(cxCurr2);
+		List<CryptoDataDto> dataList1 = new ArrayList<CryptoDataDto>(Arrays.asList(dto1, dto1, dto1));
+		List<CryptoDataDto> dataList2 = new ArrayList<CryptoDataDto>(Arrays.asList(dto2, dto2));
+		doReturn(dataList1).when(cryptoDataProvider).mapIntraDayRs(intraDay1);
+		doReturn(dataList2).when(cryptoDataProvider).mapIntraDayRs(intraDay2);
 
-		Map<String, List<CryptoDataDto>> result = mockDataProvider.collectIntraDayData(activeCxCurrs);
+		Map<String, List<CryptoDataDto>> result = cryptoDataProvider.collectIntraDayData(activeCxCurrs);
+		verify(cryptoDataProvider).callService(cxCurr1);
+		verify(cryptoDataProvider).callService(cxCurr2);
+		assertTrue(result.containsKey(cxCode1));
+		assertTrue(result.containsKey(cxCode2));
+		assertEquals(3, result.get(cxCode1).size());
+		assertEquals(2, result.get(cxCode2).size());
 
-		verify(mockDataProvider).callService(cxCurr1);
-		// verify(mockDataProvider).mapIntraDayRs(any(IntraDay.class));
+		// If any active crypto currency is provided as input, the collectIntraDayData
+		// returns an empty list
+		assertTrue(cryptoDataProvider.collectIntraDayData(Collections.emptyList()).isEmpty());
+		assertEquals(1, AlphaVantageCryptoDataProvider.getnRequests().get());
 
-//		CryptoDataProvider mockDataProvider = mock(CryptoDataProvider.class);
-//		when(mockDataProvider.callService(cxCurr1)).thenReturn(intraDay1);
-//		when(mockDataProvider.callService(cxCurr2)).thenReturn(intraDay2);
-//		doNothing().when(mockCxDataDao).saveAll(anyList());
-//
-//		Map<String, List<CryptoDataDto>> result = cxDataProvider.collectIntraDayData(activeCxCurrs);
-//		verify(mockDataProvider).callService(cxCurr1);
-//		verify(mockDataProvider).callService(cxCurr2);
-//		assertTrue(result.containsKey("CXT1"));
-//		assertTrue(result.containsKey("CXT2"));
-//		List<CryptoDataDto> dataList1 = result.get("CXT1");
-//		assertFalse(dataList1.isEmpty());
-//		assertFalse(result.get("CXT2").isEmpty());
-//		assertEquals(122.38, dataList1.get(0).getOpen());
-//		assertEquals(123.45, dataList1.get(0).getHigh());
-//		assertEquals(119.00, dataList1.get(0).getLow());
-//		assertEquals(120.54, dataList1.get(0).getClose());
-//		assertEquals(readTime, dataList1.get(0).getReadTime());
-//		assertEquals("CXT1", dataList1.get(0).getCxCurrencyDto().getCode());
-//
-//		activeCxCurrs = new ArrayList<>();
-//		CryptoCurrencyDto cxCurr3 = new CryptoCurrencyDto();
-//		cxCurr2.setCode("CXT3");
-//		activeCxCurrs.add(cxCurr3);
-//		when(mockDataProvider.callService(cxCurr3)).thenReturn(null);
-//		assertTrue(cxDataProvider.collectIntraDayData(activeCxCurrs).isEmpty());
+		// If a DataCollectException is thrown in mapIntraDayRs, that crypto currency
+		// data won´t be inserted into the result list
+		doThrow(DataCollectException.class).when(cryptoDataProvider).mapIntraDayRs(intraDay2);
+		result = cryptoDataProvider.collectIntraDayData(activeCxCurrs);
+		assertTrue(result.containsKey(cxCode1));
+		assertFalse(result.containsKey(cxCode2));
+		assertEquals(3, result.get(cxCode1).size());
 
-		// Comprobar método save. Verify?
+	}
+
+	/**
+	 * Test method for
+	 * {@link com.kssandra.ksd_task.provider.CryptoDataProvider#saveDataResult(java.util.Map, java.util.List)}.
+	 */
+	@Test
+	void testSaveDataResult() {
+
+		CryptoCurrencyDto cxCurr1 = new CryptoCurrencyDto(cxCode1);
+		CryptoCurrencyDto cxCurr2 = new CryptoCurrencyDto(cxCode2);
+
+		Map<String, LocalDateTime> lastInserted = new HashMap<>();
+		lastInserted.put(cxCode1, LocalDateTime.now().minusDays(1));
+		when(cryptoDataDao.getLastInserted(anyList())).thenReturn(lastInserted);
+
+		Map<String, List<CryptoDataDto>> dataResult = new HashMap<>();
+		List<CryptoDataDto> cxData1 = new ArrayList<>();
+		cxData1.add(new CryptoDataDto(cxCurr1, LocalDateTime.now().minusDays(2)));
+		cxData1.add(new CryptoDataDto(cxCurr1, LocalDateTime.now().minusDays(1)));
+		cxData1.add(new CryptoDataDto(cxCurr1, LocalDateTime.now()));
+
+		List<CryptoDataDto> cxData2 = new ArrayList<>();
+		cxData2.add(new CryptoDataDto(cxCurr2, LocalDateTime.now().minusDays(2)));
+		cxData2.add(new CryptoDataDto(cxCurr2, LocalDateTime.now().minusDays(1)));
+		cxData2.add(new CryptoDataDto(cxCurr2, LocalDateTime.now()));
+
+		dataResult.put(cxCode1, cxData1);
+		dataResult.put(cxCode2, cxData2);
+
+		cryptoDataProvider.saveDataResult(dataResult, new ArrayList<>(Arrays.asList(cxCurr1, cxCurr2)));
+
+		ArgumentCaptor<List<CryptoDataDto>> captor = ArgumentCaptor.forClass(List.class);
+		verify(cryptoDataDao).saveAll(captor.capture());
+
+		List<CryptoDataDto> dataToSave = captor.getValue();
+
+		// Only 2 cyrptoDataDto will be saved for CXT1 because one of the 3 initial has
+		// its readDate before lastInsertedDate
+		assertEquals(2, dataToSave.stream().filter(elem -> elem.getCxCurrencyDto().getCode().equals(cxCode1)).count());
+
+		// All 3 cryptoDataDto will be saved for CXT2 because any previous data was
+		// inserted in DB (any lastInserted date exists for CXT2).
+		assertEquals(3, dataToSave.stream().filter(elem -> elem.getCxCurrencyDto().getCode().equals(cxCode2)).count());
+
 	}
 
 }
--- src/test/java/com/kssandra/ksd_task/provider/alphavantage/AlphaVantageCryptoDataProviderTest.java
+++ src/test/java/com/kssandra/ksd_task/provider/alphavantage/AlphaVantageCryptoDataProviderTest.java
@@ -10,18 +10,18 @@ import static org.mockito.Mockito.*;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.assertj.core.util.Maps;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 
@@ -35,8 +35,6 @@ import com.kssandra.ksd_common.enums.DataProviderEnum;
 import com.kssandra.ksd_common.exception.DataCollectException;
 import com.kssandra.ksd_common.util.DateUtils;
 import com.kssandra.ksd_persistence.dao.CryptoCurrencyDao;
-import com.kssandra.ksd_persistence.dao.CryptoDataDao;
-import com.kssandra.ksd_task.provider.CryptoDataProvider;
 
 /**
  * @author aquesada
@@ -55,36 +53,14 @@ class AlphaVantageCryptoDataProviderTest {
 	@MockBean
 	AlphaVantageService avService;
 
-//	@MockBean
-//	private CryptoDataDao mockCxDataDao;
-
-//	@Autowired
-//	private CryptoDataDao cryptoDataDao;
-
-//	@MockBean
-//	CryptoDataProvider mockDataProvider;
-
-//	@MockBean
-//	AlphaVantageCryptoDataProvider mockAVProvider;
-
 	/**
 	 * Test method for
 	 * {@link com.kssandra.ksd_task.provider.alphavantage.AlphaVantageCryptoDataProvider#callService(com.kssandra.ksd_common.dto.CryptoCurrencyDto)}.
 	 */
 	@Test
-	@Disabled
 	void testCallService() {
 
-		when(avService.intraDay(anyString(), anyString(), anyString(), anyString(), anyInt(), anyString()))
-				.thenReturn(new IntraDay(null, null));
-
-		CryptoCurrencyDto cxCurrDto = new CryptoCurrencyDto();
-		cxCurrDto.setCode("CXT");
-		cxCurrDto.setName("Crypto Currency Test");
-		AVAccountDto avAccountDto = new AVAccountDto();
-		avAccountDto.setApiKey("");
-		avAccountDto.setUserId("");
-		cxCurrDto.setAvAccountDto(avAccountDto);
+		CryptoCurrencyDto cxCurrDto = new CryptoCurrencyDto("CXT", "Crypto Currency Test", new AVAccountDto(), true);
 
 		for (int i = 1; i < AlphaVantageCryptoDataProvider.getMaxRq(); i++) {
 			long initTime = System.currentTimeMillis();
@@ -94,8 +70,6 @@ class AlphaVantageCryptoDataProviderTest {
 		long initTime = System.currentTimeMillis();
 		avDataProvider.callService(cxCurrDto);
 		assertTrue((System.currentTimeMillis() - initTime) >= AlphaVantageCryptoDataProvider.getRqSleep());
-
-		assertEquals(IntraDay.class, avDataProvider.callService(cxCurrDto).getClass());
 
 	}
 
@@ -111,59 +85,31 @@ class AlphaVantageCryptoDataProviderTest {
 		assertTrue(avDataProvider.collectIntraDayData(Collections.emptyList()).isEmpty());
 		assertEquals(1, AlphaVantageCryptoDataProvider.getnRequests().get());
 
-		List<CryptoCurrencyDto> activeCxCurrs = new ArrayList<>();
-		CryptoCurrencyDto cxCurr1 = new CryptoCurrencyDto();
-		cxCurr1.setCode("CXT1");
-		activeCxCurrs.add(cxCurr1);
-		CryptoCurrencyDto cxCurr2 = new CryptoCurrencyDto();
-		cxCurr2.setCode("CXT2");
-		activeCxCurrs.add(cxCurr2);
+		String cxCurr1 = "CXT1";
+		CryptoCurrencyDto cxCurr = new CryptoCurrencyDto(cxCurr1);
+		List<CryptoCurrencyDto> activeCxCurrs = new ArrayList<CryptoCurrencyDto>(Arrays.asList(cxCurr));
 
-		Map<String, String> metaData1 = new HashMap<>();
-		metaData1.put(AlphaVantageCryptoDataProvider.getMdataCurrencyCode(), "CXT1");
-		List<SimpleCryptoCurrencyData> digitalData1 = new ArrayList<>();
-		LocalDateTime readTime = LocalDateTime.now();
-		digitalData1.add(new SimpleCryptoCurrencyData(readTime, 122.38, 123.45, 119.00, 120.54, 4562987));
-		IntraDay intraDay1 = new IntraDay(metaData1, digitalData1);
-
-		Map<String, String> metaData2 = new HashMap<>();
-		metaData2.put(AlphaVantageCryptoDataProvider.getMdataCurrencyCode(), "CXT2");
-		List<SimpleCryptoCurrencyData> digitalData2 = new ArrayList<>();
-		digitalData2.add(new SimpleCryptoCurrencyData(readTime, 222.38, 223.45, 219.00, 220.54, 4562987));
-		IntraDay intraDay2 = new IntraDay(metaData2, digitalData2);
+		Map<String, String> metaData = new HashMap<>(
+				Maps.newHashMap(AlphaVantageCryptoDataProvider.getMdataCurrencyCode(), cxCurr1));
+		List<SimpleCryptoCurrencyData> digitalData = new ArrayList<>(Arrays
+				.asList(new SimpleCryptoCurrencyData(LocalDateTime.now(), 122.38, 123.45, 119.00, 120.54, 4562987)));
+		IntraDay intraDay = new IntraDay(metaData, digitalData);
 
 		AlphaVantageCryptoDataProvider mockAVProvider = mock(AlphaVantageCryptoDataProvider.class);
-		when(mockAVProvider.callService(cxCurr1)).thenReturn(intraDay1);
-		when(mockAVProvider.callService(cxCurr2)).thenReturn(intraDay2);
-		List<CryptoDataDto> dataList1 = new ArrayList<>();
+		when(mockAVProvider.callService(cxCurr)).thenReturn(intraDay);
+
 		CryptoDataDto dto = new CryptoDataDto();
-		dto.setCxCurrencyDto(cxCurr1);
-		dataList1.add(dto);
-		List<CryptoDataDto> dataList2 = new ArrayList<>();
-		CryptoDataDto dto2 = new CryptoDataDto();
-		dto2.setCxCurrencyDto(cxCurr2);
-		dataList2.add(dto2);
-		when(mockAVProvider.mapIntraDayRs(intraDay1)).thenReturn(dataList1);
-		when(mockAVProvider.mapIntraDayRs(intraDay2)).thenReturn(dataList2);
-		// doNothing().when(mockCxDataDao).saveAll(anyList());
+		dto.setCxCurrencyDto(cxCurr);
+		List<CryptoDataDto> dataList = new ArrayList<CryptoDataDto>(Arrays.asList(dto, dto, dto));
+		when(mockAVProvider.mapIntraDayRs(intraDay)).thenReturn(dataList);
 
 		doCallRealMethod().when(mockAVProvider).collectIntraDayData(activeCxCurrs);
 
 		Map<String, List<CryptoDataDto>> result = mockAVProvider.collectIntraDayData(activeCxCurrs);
-		verify(mockAVProvider).callService(cxCurr1);
-		verify(mockAVProvider).callService(cxCurr2);
-		assertTrue(result.containsKey("CXT1"));
-		assertTrue(result.containsKey("CXT2"));
-		assertFalse(result.get("CXT1").isEmpty());
-		assertFalse(result.get("CXT2").isEmpty());
-
-//
-//		activeCxCurrs = new ArrayList<>();
-//		CryptoCurrencyDto cxCurr3 = new CryptoCurrencyDto();
-//		cxCurr2.setCode("CXT3");
-//		activeCxCurrs.add(cxCurr3);
-//		when(mockAVProvider.callService(cxCurr3)).thenReturn(null);
-//		assertTrue(avDataProvider.collectIntraDayData(activeCxCurrs).isEmpty());
+		verify(mockAVProvider).callService(cxCurr);
+		assertTrue(result.containsKey(cxCurr1));
+		assertFalse(result.get(cxCurr1).isEmpty());
+		assertFalse(result.containsKey("CXT2"));
 
 	}
 
@@ -176,6 +122,7 @@ class AlphaVantageCryptoDataProviderTest {
 	@Test
 	void testMapIntraDayRs() throws DataCollectException {
 
+		// Testing exceptions
 		assertThrows(DataCollectException.class, () -> avDataProvider.mapIntraDayRs(null),
 				"Error mapping response from AlphaVantage: Result from AV is null");
 
@@ -207,10 +154,9 @@ class AlphaVantageCryptoDataProviderTest {
 				() -> avDataProvider.mapIntraDayRs(new IntraDay(metaData3, digitalData2)),
 				"Error mapping response from AlphaVantage: DigitalData is null or empty");
 
+		// Testing OK case
 		String cxCode = "CXT";
-		CryptoCurrencyDto cxCurrDto = new CryptoCurrencyDto();
-		cxCurrDto.setCode(cxCode);
-		cxCurrDto.setName("Crypto Currency Test");
+		CryptoCurrencyDto cxCurrDto = new CryptoCurrencyDto(cxCode);
 		List<SimpleCryptoCurrencyData> digitalData3 = new ArrayList<>();
 		LocalDateTime readtime = LocalDateTime.now();
 		digitalData3.add(new SimpleCryptoCurrencyData(readtime, 122.38, 123.45, 119.00, 120.54, 4562987));
@@ -227,6 +173,7 @@ class AlphaVantageCryptoDataProviderTest {
 		assertEquals(readtime.atZone(ZoneId.of("GMT")).withZoneSameInstant(ZoneId.of(DateUtils.DEFAULT_OFFSET))
 				.toLocalDateTime(), resultData.getReadTime());
 
+		// Generic exception will be throw if digitalData list is empty
 		List<SimpleCryptoCurrencyData> digitalData4 = new ArrayList<>();
 		digitalData4.add(new SimpleCryptoCurrencyData(null, 122.38, 123.45, 119.00, 120.54, 4562987));
 		assertThrows(Exception.class, () -> avDataProvider.mapIntraDayRs(new IntraDay(metaData3, digitalData4)));
