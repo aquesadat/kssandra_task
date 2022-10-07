109c0b0971f763fbf034ff6ac10f21f8ca55c055
aquesada <aquesadatello@gmail.com>
2022-10-07 20:07:24 +0200

    Set dependency projects as libraries

--- src/main/java/com/kssandra/ksd_task/KsdTaskApplication.java
+++ src/main/java/com/kssandra/ksd_task/KsdTaskApplication.java
@@ -2,13 +2,17 @@ package com.kssandra.ksd_task;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 import com.kssandra.ksd_common.util.Generated;
 
 @SpringBootApplication
 @ComponentScan({ "com.kssandra" })
+@EntityScan("com.kssandra.ksd_persistence.domain")
+@EnableJpaRepositories("com.kssandra.ksd_persistence.repository")
 @EnableScheduling
 @Generated
 public class KsdTaskApplication {
